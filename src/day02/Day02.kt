5ae129a1f07a6302ebc296b394897ad722888aa0
Vlad Potapov <v.potapov@mielooandalexander.com>
2022-12-02 14:31:38 +0100

    day02 part2 solution

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -38,30 +38,50 @@ fun main() {
             The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
             plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
              */
-    fun part1(input: List<String>): Int {
-        return input
-            .map {
-                val (o1, o2) = it.split(" ")
-                Pair(parseMove(o1), parseMove(o2))
-            }
-//            .also { println(it) }
-            .map { calcRoundScore(it) }
-//            .also { println(it) }
-            .sum()
+    fun part1(input: List<String>): Int = input
+        .map {
+            val (o1, o2) = it.split(" ")
+            Pair(parseMove(o1), parseMove(o2))
+        }
+        .sumOf { calcRoundScore(it) }
+
+
+    fun parseExpectedResult(o2: String): RoundResult {
+        return when (o2) {
+            "X" -> LOST
+            "Y" -> DRAW
+            "Z" -> WIN
+            else -> throw IllegalArgumentException("Unknown code $o2")
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun predictMove(move: Move, expectedResult: RoundResult): Move {
+        val comparator = ResultComparator()
+        return when (expectedResult) {
+            LOST -> Move.values().single { comparator.compare(move, it) == -1 }
+            DRAW -> Move.values().single { comparator.compare(move, it) == 0 }
+            WIN -> Move.values().single { comparator.compare(move, it) == 1 }
+        }
     }
+
+    /*
+                Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+                 */
+    fun part2(input: List<String>): Int = input
+        .map {
+            val (o1, o2) = it.split(" ")
+            Pair(parseMove(o1), predictMove(parseMove(o1), parseExpectedResult(o2)))
+        }
+        .sumOf { calcRoundScore(it) }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day02/test_input")
     check(part1(testInput) == 15)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 12)
 
     val input = readInput("day02/input")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 
 }
 
@@ -88,5 +108,4 @@ class ResultComparator : Comparator<Move> {
             else -> -1
         }
     }
-
 }
