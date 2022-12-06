e9e79458b801675191c031a1f1d963590b2cd492
Vlad Potapov <v.potapov@mielooandalexander.com>
2022-12-06 18:48:13 +0100

    day06 part1 solution

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -1,10 +1,17 @@
 package day06
 
 import readInput
+import kotlin.streams.asSequence
 
+/**
+ * --- Day 6: Tuning Trouble ---
+ */
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val size = 4
+        return input.single()
+            .windowed(size)
+            .indexOfFirst { it.toSet().size == size } + size
     }
 
     fun part2(input: List<String>): Int {
@@ -13,10 +20,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day06/test_input")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    check(part1(testInput) == 7)
+//    check(part2(testInput) == 1)
 
     val input = readInput("day06/input")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
